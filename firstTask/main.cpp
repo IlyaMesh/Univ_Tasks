9b9c6ab8f1eb629a597c42b22dabfaa8ea897841
Ilya Mesheryakov <mescheryakov.i.g@gmail.com>
2020-03-03 11:43:53 +0300

    first task done

--- firstTask/main.cpp
+++ firstTask/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
+#include<string.h>
 #include "mystrchr.h"
 
 using namespace std;
 
 int main() {
-    char* inputString = new char[128];
+    char *inputString = new char[128];
     char symbol;
-    cout << "Input string \n";
-    scanf("%s",inputString);
-    cout << "Input symbol that u wanna find \n";
-    cin >> symbol;
-    cout << mystrchr(inputString,symbol);
-    return 0;
+    while (true) {
+        cout << "Enter q to quit \n"<<"-------------- \n"<< "Enter the string \n";
+        scanf("%s", inputString);
+        if (!strcmp(inputString,"q")) {
+            cout << "Quit";
+            return 0;
+        }
+        cout << "Enter symbol that you wanna find \n";
+        cin >> symbol;
+        char *result = mystrchr(inputString, symbol);
+        result == NULL ? cout << "Symbol wasn't found\n" : cout <<"Answer: "<<result<<'\n';
+    }
 }
--- firstTask/mystrchr.cpp
+++ firstTask/mystrchr.cpp
@@ -6,7 +6,7 @@
 
 char * mystrchr (const char * str, int character){
     int i = 0;
-    while (str[i] && str[i] != character) ++i;
+    while (str[i] && str[i] != character) i++;
     return character == str[i] ? (char*)str + i : NULL;
 }
 
